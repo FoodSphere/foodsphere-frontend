f7e0495ee5f1cb2fd4b7e80f86afcc27aa6d8780
Thanathat Pinthu <thanathatp2003@gmail.com>
2025-10-02 18:52:50 +0700

    feat: login page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,17 @@
+"use client";
+import { usePathname } from "next/navigation";
+
 import { Sidebar } from "../components/sidebar/Sidebar";
 
-export default function layout({
+export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const pathname = usePathname();
+  const isLoginPage = pathname === "/login";
+
   return (
     <div className="flex flex-row">
-      <Sidebar/>
+      {!isLoginPage && <Sidebar />}
       {children}
     </div>
   );
---
+++ app/(main)/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginRender from "@/app/features/main/login/Index";
+
+const page = () => {
+  return (
+    <div className="w-full">
+      <LoginRender />
+    </div>
+  );
+};
+
+export default page;
