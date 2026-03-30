689bdf689ccf0b1cf98d42cd837cf0b13142ec07
Tanawat Laong <110480443+kodpleum@users.noreply.github.com>
2026-03-31 03:35:05 +0700

    fix(table): fix multiple variable declaration.

--- app/features/main/table/Index.tsx
+++ app/features/main/table/Index.tsx
@@ -411,21 +411,20 @@ const TableRender = () => {
     const paymentMethod = searchParams.get("payment_method");
     const tableId = searchParams.get("table_id");
     const billId = searchParams.get("bill_id");
-    const tableId = searchParams.get("table_id");
     const cancel = searchParams.get("cancel");
     const paymentId = searchParams.get("payment_id");
 
     if (tableId && billId) {
       console.log("tableId", tableId);
       console.log("billId", billId);
-        const currentTableData = {
-          id: tableId,
-          name: currentTable?.name || "",
-          hasCustomers: true,
-          billId: billId,
-        };
-        openTable(currentTableData);
-        router.replace("/table")
+      const currentTableData = {
+        id: tableId,
+        name: currentTable?.name || "",
+        hasCustomers: true,
+        billId: billId,
+      };
+      openTable(currentTableData);
+      router.replace("/table");
     }
 
     const clearQueryParams = () => {
